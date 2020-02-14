02ced66f2a35624856273e7c54b22e0a1812d8cc
takefumi24 <t_ohba24@yahoo.co.jp>
2020-02-14 15:07:00 +0900

    create_automatic-update-function1

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,5 @@
 $(function () {
+
   function buildHTML(message) {
     if (message.image) {
       var html =
@@ -39,6 +40,31 @@ $(function () {
       return html;
     };
   }
+
+  var reloadMessages = function () {
+    var last_message_id = $(".messages__message:last").data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+
+    .done(function (messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        messages.forEach(function(message) {
+          insertHTML = buildHTML(message);
+          $(".messages").append(insertHTML);
+        });
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+      }
+    })
+    .fail(function () {
+      alert("メッセージ送信に失敗しました");
+    });
+  };
+
   $('#new_message').on('submit', function (e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -47,19 +73,25 @@ $(function () {
       url: url,
       type: "POST",
       data: formData,
-      dataType: 'json',
+      dataType: "json",
       processData: false,
       contentType: false
     })
-      .done(function (data) {
-        var html = buildHTML(data);
-        $('.messages').append(html);
-        $('form')[0].reset();
-        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
-        $('.submit-btn').attr('disabled', false);
-      })
-      .fail(function () {
-        alert("メッセージ送信に失敗しました");
-      });
+
+    .done(function(data) {
+      var html = buildHTML(data);
+      $(".messages").append(html);
+      $("form")[0].reset();
+      $(".messages").animate({ scrollTop: $(".messages")[0].scrollHeight });
+      $(".submit-btn").attr("disabled", false);
+    })
+
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
   })
+
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
